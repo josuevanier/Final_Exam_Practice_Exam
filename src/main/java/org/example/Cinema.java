7354697aad54b32bacab90aea0d96edbef3ecca3
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-04-21 08:30:23 -0400

    Using linked hash set and the steam methods of java

---
+++ src/main/java/org/example/Cinema.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.concurrent.ConcurrentMap;
+
+public class Cinema {
+    String Genre;
+    double rating;
+    double revenue;
+    String Director;
+    String year;
+
+    public Cinema(String genre, double rating, double revenue, String director, String year) {
+        Genre = genre;
+        this.rating = rating;
+        this.revenue = revenue;
+        Director = director;
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Cinema{" +
+                "Genre='" + Genre + '\'' +
+                ", rating=" + rating +
+                ", revenue=" + revenue +
+                ", Director='" + Director + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        LinkedHashSet<Cinema> cinemas = new LinkedHashSet<>();
+
+        cinemas.add(new Cinema("Action", 2.4,1000.5,"Joseph","2023"));
+        cinemas.add(new Cinema("Comedy", 2.4,1000.4,"Tosue","2024"));
+        cinemas.add(new Cinema("Dra,", 2.4,1000.6,"Jos","2025"));
+
+      cinemas.stream().sorted(Comparator.comparing(Cinema:: getGenre)).forEach(System.out::print);
+
+      Cinema maxPorfitObjectCinema = cinemas.stream().max(Comparator.comparing(Cinema:: getRevenue)).orElse(null);
+        System.out.println();
+        System.out.println(maxPorfitObjectCinema);
+
+        String directorName = cinemas.stream().max(Comparator.comparing(Cinema::getRevenue)).map(Cinema::getDirector).orElse(null);
+
+        System.out.println(directorName);
+    }
+}
