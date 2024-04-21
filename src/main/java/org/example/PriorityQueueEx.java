50aa99e80a9c70ccd97f1fd53ff63393b16f62ef
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-04-21 13:06:39 -0400

    So priority queue when ever using sorting in java the class that you sorting must have impliments comparable so that the other methods from comparator can overides the method in the actual class. In my case the class of employee

--- src/main/java/org/example/PriorityQueueEx.java
+++ src/main/java/org/example/PriorityQueueEx.java
@@ -1,32 +1,87 @@
 package org.example;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Employee implements Comparable<Employee>{
+    String id;
+    int age;
+    String firstName;
+    String lastName;
+
+    public Employee(String id, int age, String firstName, String lastName)  {
+        this.id = id;
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", age=" + age +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return  this.lastName.compareTo(o.getFirstName());
+    }
+
+    static class SortingByAge implements Comparator<Employee>{
+
+
+        @Override
+        public int compare(Employee o1, Employee o2) {
+
+            if(o1.getAge() > o2.getAge()) return 1;
+            else if (o1.getAge() == o2.getAge()) return 0;
+            else return  -1;
+        }
+
+    }
+    static class SortingByName implements  Comparator<Employee>{
+
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return Integer.compare(o1.age, o2.getAge());
+        }
+    }
+}
 
 public class PriorityQueueEx {
     public static void main(String[] args) {
-        PriorityQueue<Integer> integers = new PriorityQueue<>();
 
-        integers.add(182);
-        integers.add(2);
-        integers.add(3);
-        integers.add(9);
-        integers.add(15);
-        integers.add(30);
+        PriorityQueue<Employee> employees = new PriorityQueue<>();
 
-        PriorityQueue<Integer> integersDivide3and5 = new PriorityQueue<>();
+        employees.add(new Employee("DDO1",14,"A","F"));
+        employees.add(new Employee("DDO2",15,"B","F"));
+        employees.add(new Employee("DDO3",16,"D","F"));
 
-        for(Integer integer : integers){
-            if(integer % 3 == 0 && integer % 5 == 0){
-                integersDivide3and5.add(integer);
-            }
-        }
+        PriorityQueue<Employee> sortedByAge = new PriorityQueue<>(new Employee.SortingByAge());
+         sortedByAge.addAll(employees);
+        System.out.println(sortedByAge);
 
-            System.out.println(integersDivide3and5);
 
-        while(!integersDivide3and5.isEmpty()){
-            System.out.println(integersDivide3and5.poll()); // retuns the element with priority by natural ordering if no comparator is given
-        }
 
     }
 }
